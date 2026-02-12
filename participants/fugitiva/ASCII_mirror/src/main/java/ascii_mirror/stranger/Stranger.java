c37d988579eeb26ece3720471c7c38e3f1c2b78f
fugitiva <saralamadrid@outlook.com>
2026-02-12 19:49:07 +0100

    Title: participant - fugitiva 1. Add new class ScannerInput 2. Add a new class ScannerOrder 3. Add Scanner 4. update Stranger

--- participants/fugitiva/ASCII_mirror/src/main/java/ascii_mirror/stranger/Stranger.java
+++ participants/fugitiva/ASCII_mirror/src/main/java/ascii_mirror/stranger/Stranger.java
@@ -4,8 +4,9 @@ package ascii_mirror.stranger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
-
+@Order(1)
 @Component
 public class Stranger implements CommandLineRunner {
 
